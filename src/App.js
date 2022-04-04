a2f07820076edfaa25004ae2861ef16af6386714
vanz1402 <ije.malong@gmail.com>
2022-04-04 13:46:20 +0800

    time diff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import SpeedVelocity from './SpeedVelocity';
+import InputField from './inputfield'
+import React, { Component, useRef } from 'react'
 
 function App() {
+  const nameForm = useRef(null)
+
+  const timer1 = ()=>{
+    // show the user input value to console
+    const form = nameForm.current
+    const timediff = form['time2'] - form['time1'].value
+     alert(`${timediff}`)
+  };
+
   return (
-    <div className="App">
-      <SpeedVelocity/>
+    <div className='App'>
+      <h1>Speed Velocity</h1>
+      <form ref={nameForm}>
+       <InputField label={'Release Time: '} name={'time1'}/>
+       <InputField label={'Arrival Time: '} name={'time2'}/>
+      </form>
+      <button onClick={timer1}>Compute Time</button>
     </div>
-    
   );
 }
 
